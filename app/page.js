93a74bddfed54d60ff0069b3b115d0ab6184c0ec
cristian <cristian.usuga.ardila@gmail.com>
2023-08-03 21:50:04 -0500

    inicial proyecto

--- app/page.js
+++ app/page.js
@@ -3,8 +3,9 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    
+
     <main className={styles.main}>
+      
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
